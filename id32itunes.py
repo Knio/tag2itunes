6287936c611b8f8ecd462b49abcbe1758c9ac01e
Tom Flanagan <theknio@gmail.com>
2010-01-02 00:12:34 -0700

    sdf

--- id32itunes.py
+++ id32itunes.py
@@ -1,3 +1,24 @@
+# Script to import MP3 metadata from ID3 tags into an iTunes database,
+# because iTunes is too retarded to read/write to standard formats
+# and uses its own proprietarty format.
+#
+#
+# Directions
+# ==========
+#
+# 0. Have file statistics (ratings, playcounts) in ID3 tags that you want in iTunes (This may take 4 hours)
+# 1. Import all your music into iTunes normally. iTunes will ignore the data you want.
+# 2. Close iTunes. Do not open it again until step 6.
+# 3. Copy `/USERNAME/Music/iTunes/iTunes Music Library.xml` and `/USERNAME/Music/iTunes/iTunes Library` to a safe place
+# 4. DELETE `/USERNAME/Music/iTunes/iTunes Music Library.xml` and `/USERNAME/Music/iTunes/iTunes Library`
+#    This might fuck up other things that iTunes knows.
+# 5. run id32itunes.py `/safeplace/iTunes Music Library.xml` (This may take 4 hours)
+# 6. open iTunes. AAAHH, all your data is gone!
+# 7. in iTunes, go File->Library->Import Playlist, select `/safeplace/iTunes Music Library.xml` (This may take 4 hours)
+# 8. Done!
+# 9. if it fucks up, restore `/USERNAME/Music/iTunes/iTunes Music Library.xml` and `/USERNAME/Music/iTunes/iTunes Library` from backup
+#
+
 import sys
 import os
 import re
@@ -44,36 +65,47 @@ class Syncer(object):
   
   def __init__(self):
     self.rootfolder = ('file://localhost/Users/Tom/Music/', 'Z:/Tom/Media/Music/')    
+    self.n_rating    = 0
+    self.n_playcount = 0
+    self.n_failed    = 0
+    
   
   def processtrack(self, e):
     d = parsedict(e)
-    filepath = decode(urllib.unquote(d['Location'].text))
-    
-    #print ('\b'*80),
-    print ('P: %s' % filepath[40:110])
     
     try:
+      filepath = decode(urllib.unquote(d['Location'].text))
+      print ('P: %s' % os.path.basename(filepath))    
       id3 = ID3(filepath.replace(*self.rootfolder))
     except:
-      print 'E: Failed to open ID3 tags'
+      print 'E: Failed to open file'
+      self.n_failed += 1
       return
-      
-    try:
-      rating = self.r2itunes[int(id3['TXXX:rating'].text[0])]
-      setdict(e, d, 'Rating', 'integer', str(rating))
-      print '      Rating = %s' % rating
-    except KeyError:
-      pass
     
+    def gettag(tags):
+      v = None
+      for t in tags:
+        v = v or id3.get(t)
+      if v: v = v.text[0]
+      return v      
+    
+    #      <key>Rating</key><integer>20</integer></dict>
     #      <key>Play Count</key><integer>1</integer>
     #      <key>Play Date</key><integer>3341475263</integer>
-    #      <key>Play Date UTC</key><date>2009-11-19T18:34:23Z</date>
-    try:
-      playcount = int(id3['TXXX:play_count'].text[0])
-      setdict(e, d, 'Play Count', 'integer', str(playcount))
-      print '      Play Count = %s' % playcount
-    except KeyError:
-      pass    
+    #      <key>Play Date UTC</key><date>2009-11-19T18:34:23Z</date>      
+    v = gettag(['TXXX:rating', 'TXXX:RATING'])
+    if v and int(v):
+      v = self.r2itunes[int(v)]
+      setdict(e, d, 'Rating', 'integer', str(v))
+      self.n_rating += 1
+      print '      Rating = %r' % v
+    
+    v = gettag(['TXXX:play_count'])
+    if v:
+      setdict(e, d, 'Play Count', 'integer', str(int(v)))
+      self.n_playcount += 1
+      print '      Play Count = %r' % v
+      
     
   def walklibrary(self, root):
     d = parsedict(root[0])
@@ -102,9 +134,9 @@ class Syncer(object):
     print 'Writing library..' 
     self.library.write(self.library_name)
     print 'Done!'
-    
-  
-  
+    print 'Wrote %5d ratings'     % self.n_rating
+    print 'Wrote %5d play counts' % self.n_playcount
+    print 'Failed to read %d files' % self.n_failed
 
 
 if __name__ == '__main__':
